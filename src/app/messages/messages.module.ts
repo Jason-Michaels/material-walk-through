5ff3d36d6dc6536fe90eb8fd0006f096b8c60e4e
Jason Michaels <jason.michaels721@gmail.com>
2019-04-17 15:46:59 +0000

    test card add

--- src/app/messages/messages.module.ts
+++ src/app/messages/messages.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { MessagesRoutingModule } from './messages-routing.module';
 import { MessageListComponent } from './message-list/message-list.component';
+import { MessageNewComponent } from './message-new/message-new.component';
 
 @NgModule({
-  declarations: [MessageListComponent],
+  declarations: [MessageListComponent, MessageNewComponent],
   imports: [
     CommonModule,
     MessagesRoutingModule
