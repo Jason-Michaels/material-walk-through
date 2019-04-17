c5a76055c1f0abfaefa6352e463471f9e047987a
Jason Michaels <jason.michaels721@gmail.com>
2019-04-17 14:29:00 +0000

    some colors and nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-//Material Section
+// Material Section
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
+
+// Routing
+import {AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -15,10 +19,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+
+    // Material
     BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatToolbarModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
