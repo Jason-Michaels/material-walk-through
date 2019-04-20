c74d88c39a7771352783b21605690b724ffdc1b2
Jason Michaels <jason.michaels721@gmail.com>
2019-04-20 16:58:19 +0000

    wip data tables

--- src/app/orders/order-list/order-list.component.ts
+++ src/app/orders/order-list/order-list.component.ts
@@ -1,4 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort, MatTableDataSource } from '@angular/material';
+
+const ELEMENT_DATA = [
+  {
+    orderDate: new Date(),
+    orderNumber: 100,
+    total: 29.99,
+    descriptions: '800g of tuna',
+    isChecked: false
+  },
+  {
+    orderDate: new Date(),
+    orderNumber: 101,
+    total: 19.99,
+    descriptions: '500g of tuna',
+    isChecked: false
+  },
+  {
+    orderDate: new Date(),
+    orderNumber: 102,
+    total: 59.99,
+    descriptions: '1kg of tuna',
+    isChecked: false
+  }
+];
 
 @Component({
   selector: 'app-order-list',
@@ -6,10 +31,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-list.component.css']
 })
 export class OrderListComponent implements OnInit {
+  displayedColumns: string[] = ['action', 'orderNumber', 'orderDate', 'description', 'total'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor() { }
 
   ngOnInit() {
+    this.dataSource.sort = this.sort
   }
 
 }
